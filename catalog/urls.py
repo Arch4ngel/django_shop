35acb4143df21c3770eede88ca5bba5ff6177505
Arch4ngel <mishacool4@gmail.com>
2023-11-30 20:05:48 +0300

    done homework 4

--- catalog/urls.py
+++ catalog/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
 
-from catalog.views import catalog, contacts
+from catalog.views import catalog, contacts, product_info
 
 app_name = 'catalog'
 
 urlpatterns = [
     path('', catalog),
-    path('contacts/', contacts)
+    path('contacts/', contacts),
+    path('product/<int:pk>/', product_info)
 ]
--- catalog/views.py
+++ catalog/views.py
@@ -1,16 +1,41 @@
 from django.shortcuts import render
+from catalog.models import Category, Product
 
 # Create your views here.
 
 
 def catalog(request):
-    return render(request, 'catalog/home.html')
+    product_list = Product.objects.all()
+    context = {
+        'object_list': product_list,
+        'title': 'Каталог'
+    }
+    return render(request, 'catalog/templates/index.html', context)
 
 
 def contacts(request):
+    context = {'title': 'Контакты'}
     if request.method == 'POST':
         name = request.POST.get('name')
         phone = request.POST.get('phone')
         message = request.POST.get('message')
         print(f'{name} ({phone}): {message}')
-    return render(request, 'catalog/contacts.html')
+    return render(request, 'catalog/templates/contacts.html', context)
+
+
+def product_info(request, pk):
+    product = Product.objects.get(pk=pk)
+
+    context = {
+        'object': product,
+        'title': product.name,
+        'category': product.category
+    }
+    return render(request, 'catalog/templates/product-info.html', context)
+
+# def categories(request):
+#     context = {
+#         'object_list': Category.objects.all(),
+#         'title': 'Продукты'
+#     }
+#     return render(request, 'main/category_list.html', context)
