5f05521bea42bd137ff1bd456b7f2d2705ab812c
Arch4ngel <mishacool4@gmail.com>
2023-12-23 01:28:55 +0300

    done homework 6

---
+++ catalog/forms.py
@@ -0,0 +1,36 @@
+from django import forms
+from catalog.models import Product, Version
+
+
+class StyleFormMixin:
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        for field_name, field in self.fields.items():
+            field.widget.attrs['class'] = 'form-control'
+
+
+class ProductForm(StyleFormMixin, forms.ModelForm):
+
+    class Meta:
+        model = Product
+        fields = '__all__'
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        for field_name, field in self.fields.items():
+            field.widget.attrs['class'] = 'form-control'
+
+    def clean_name(self):
+        stop_words = ['казино', 'криптовалюта', 'крипта', 'биржа', 'дешево', 'бесплатно', 'обман', 'полиция', 'радар']
+        cleaned_data = self.cleaned_data.get('name')
+        for word in stop_words:
+            if word in cleaned_data:
+                raise forms.ValidationError('Ошибка, связанная с именем продукта')
+        return cleaned_data
+
+
+class VersionForm(StyleFormMixin, forms.ModelForm):
+
+    class Meta:
+        model = Version
+        fields = '__all__'
--- catalog/models.py
+++ catalog/models.py
@@ -21,8 +21,8 @@ class Product(models.Model):
     image = models.ImageField(upload_to='products/', verbose_name='Изображение', **NULLABLE)
     category = models.ForeignKey(Category, on_delete=models.CASCADE, verbose_name='Категория')
     price = models.IntegerField(verbose_name='Цена за штуку')
-    date_created = models.DateTimeField(verbose_name='Дата создания')
-    date_modified = models.DateTimeField(verbose_name='Дата изменения')
+    date_created = models.DateTimeField(auto_now_add=True, verbose_name='Дата создания')
+    date_modified = models.DateTimeField(auto_now_add=True, verbose_name='Дата изменения')
 
     def __str__(self):
         return f'{self.name} ({self.category})'
@@ -30,3 +30,17 @@ class Product(models.Model):
     class Meta:
         verbose_name = 'продукт'
         verbose_name_plural = 'продукты'
+
+
+class Version(models.Model):
+    product = models.ForeignKey(Product, on_delete=models.CASCADE, verbose_name='Версия')
+    number = models.IntegerField(verbose_name='Номер версии')
+    name = models.CharField(max_length=50, verbose_name='Имя версии')
+    is_active = models.BooleanField(verbose_name='Активна', default=False)
+
+    def __str__(self):
+        return f'{self.name}'
+
+    class Meta:
+        verbose_name = 'версия'
+        verbose_name_plural = 'версии'
